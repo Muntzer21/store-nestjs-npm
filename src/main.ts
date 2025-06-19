b96e36f57e487380e23292ed464c7cfd5a9bea26
muntazer <muntzrdeveloper@gmail.com>
2025-06-19 05:33:52 +0300

    add cors to 127.0.0.1:5502 to can request from vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://127.0.0.1:5502', // Allow your local development server
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed methods
+    credentials: true, // If you're sending cookies/authentication headers
+  });
   app.use(helmet());
   // app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(
