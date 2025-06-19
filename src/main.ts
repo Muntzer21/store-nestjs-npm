9539cb72848993d27a702471965f1ce3b235f9dc
muntazer <muntzrdeveloper@gmail.com>
2025-06-19 04:57:35 +0300

    edit vercel.json and remoce api v1

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  app.setGlobalPrefix('api/v1')
+  // app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
